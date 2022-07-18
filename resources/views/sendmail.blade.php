05e55988a12c029d2e1ddd196ff2bf78c60f8b7d
Joel <joel@magdalenamedio.net>
2022-07-18 16:57:25 -0500

    se resuelve problema con el captcha y se valida correctamente

--- resources/views/sendmail.blade.php
+++ resources/views/sendmail.blade.php
@@ -7,6 +7,22 @@ $subject    = @trim(stripslashes($_POST['subject']));
 $message    = @trim(stripslashes($_POST['message'])); 
 $to   		= 'joelmarquez@gmail.com';
 
+$ip = $_SERVER['REMOTE_ADDR'];
+$captcha = $_POST['g-recaptcha-response'];
+$secretkey = "6LfA06QaAAAAANF57k_3XFuE81jv1e9iLZCHizJl";
+
+// Enviamos una URL para la validación
+$respuesta = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretkey&response=$captcha&remoteip=$ip");
+
+$atributos = Json_decode($respuesta, TRUE);
+
+if(!$atributos['success']){
+    echo '<center>' . "<p> Hubo un problema: Verifica el Captcha. </p>" . '<center>' ;
+    echo '<a href="javascript:history.back()">' . "<p> Volver Atrás </p>" . '</a>';
+    header("refresh:10;url=https://joelmarquez.net");
+    die();
+}
+
 $headers   = [
     'MIME-Version' => 'MIME-Version: 1.0',
     'Content-type' => 'text/plain; charset=UTF-8',
